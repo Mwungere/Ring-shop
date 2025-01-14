9a1c01529d7e8e0a77d0e8bef7f1cc2961d56dac
Mwungere <mwungereelite56@gmail.com>
2025-01-14 13:13:08 +0200

    responsiveness

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -2,7 +2,7 @@ import { Leva } from 'leva';
 import { Suspense, useRef, useState } from 'react';
 import { Canvas, useFrame } from '@react-three/fiber';
 import { useMediaQuery } from 'react-responsive';
-import { ArrowLeft, Check, ChevronLeft, Edit, Ellipsis, ExternalLink, Info, Pencil } from 'lucide-react';
+import { ArrowLeft, Check, ChevronLeft, Cross, Edit, Ellipsis, ExternalLink, Info, Menu, Pencil, X } from 'lucide-react';
 import { PerspectiveCamera, SpotLight } from '@react-three/drei';
 
 import CanvasLoader from '../components/Loading.jsx';
@@ -38,12 +38,15 @@ const RotatingModel = ({ scale, position, rotation, activeRing, activeBox }) => 
   );
 };
 
+
+
 const Hero = () => {
   const isMobile = useMediaQuery({ maxWidth: 768 });
   const navigate = useNavigate();
   const [isEn, setIsEn] = useState(true);
   const [activeRing, setActiveRing] = useState(0);
   const [activeBox, setActiveBox] = useState(0);
+  const [menuOpen, setMenuOpen] = useState(false);
 
   const handleSetActiveRing = () => {
     console.log('active ring', activeRing);
@@ -62,8 +65,11 @@ const Hero = () => {
   };
   return (
     <section
-      className="min-h-screen w-full flex relative justify-center items-center bg-gradient-to-r from-black-200 via-zinc-800 to-zinc-700"
+      className={`min-h-screen w-full flex relative justify-center items-center ${menuOpen? 'bg-white lg:bg-gradient-to-r from-black-200 via-zinc-800 to-zinc-700': 'bg-gradient-to-r from-black-200 via-zinc-800 to-zinc-700'}`}
       id="home">
+        {!menuOpen && <div onClick={() => setMenuOpen(true)} className=' absolute top-2 left-6 flex cursor-pointer lg:hidden z-50'><Menu className=' text-white' /></div>}
+        {menuOpen && <div onClick={() => setMenuOpen(false)} className=' absolute top-2 left-6 flex cursor-pointer lg:hidden z-50'><X className=' text-black' /></div>}
+
       <div className="w-full h-screen flex justify-center items-center overflow-auto">
         <Canvas className="max-h-[600px] max-w-[800px]">
           <Suspense fallback={<CanvasLoader />}>
@@ -86,7 +92,7 @@ const Hero = () => {
         </Canvas>
       </div>
 
-      <div className="min-w-[500px] max-w-[500px] h-[90%] bg-white rounded-md mr-2 relative">
+      <div className={` ${!menuOpen ? ' hidden lg:block transition-all duration-200' : ''} z-40 min-w-full lg:min-w-[500px] max-w-[500px] h-[80%] bg-white rounded-md mr-2 relative`}>
         <Swiper
           onSwiper={setSwiperRef}
           virtual
@@ -94,8 +100,10 @@ const Hero = () => {
           navigation={{
             nextEl: '.swiper-button-next',
             prevEl: '.swiper-button-prev',
+
           }}
-          className="mySwiper">
+          className=''
+          >
           <SwiperSlide virtualIndex={0}>
             <div className=" w-full p-2 flex justify-end">
               <Ellipsis
@@ -109,7 +117,7 @@ const Hero = () => {
               <p className=" text-gray-500">${(parseFloat(items.rings[activeRing].price) + parseFloat(items.boxes[activeBox].price)).toFixed(2)}</p>
             </div>
 
-            <div className=" mt-[80px] flex flex-col gap-5 px-3 mb-60">
+            <div className=" mt-[80px] flex flex-col gap-5 px-3 mb-32 lg:mb-60">
               <div
                 onClick={() => slideTo(2)}
                 className="flex justify-between items-center px-2 hover:border hover:border-gray-200 py-1 rounded-md transition-all duration-100 my-2 group hover:cursor-pointer">
@@ -157,10 +165,10 @@ const Hero = () => {
               </button>
             </div>
           </SwiperSlide>
-          <SwiperSlide virtualIndex={1} className=" flex flex-col justify-center items-center p-4">
+          <SwiperSlide virtualIndex={1} className="relative flex flex-col justify-center items-center px-4 py-2 lg:p-4">
             <ChevronLeft
               onClick={() => slideTo(0)}
-              className="cursor-pointer self-start m-5 size-6 text-black-300 top-1"
+              className="cursor-pointer self-end lg:self-start lg:m-5 size-6 text-black-300 top-1"
             />
             <h2 className=" font-medium font-sans text-3xl text-gray-700 mt-10">{isEn ? 'menu' : 'Menü '}</h2>
             <hr className=" w-full mt-20 bg-gray-900" />
@@ -204,8 +212,8 @@ const Hero = () => {
               </div>
             </div>
 
-            <div className=" w-full mt-[300px] flex justify-center items-center">
-              <p className=" text-sm text-center font-medium text-gray-400">
+            <div className=" w-full mt-[350px] md:mt-[380px] lg:mt-[250px] flex justify-center items-center">
+              <p className=" absolute bottom-[280px] lg:bottom-20 lg:relative text-sm text-center font-medium text-gray-400">
                 Designed + Developed by soehne.co <br /> © 2024, Lockcard.
               </p>
             </div>
@@ -214,14 +222,14 @@ const Hero = () => {
           <SwiperSlide virtualIndex={2} className=" flex flex-col justify-center items-center p-4">
             <h2 className=" font-medium text-xl">Ring</h2>
 
-            <div className=" w-full mt-[80px] flex flex-col mb-20 gap-2">
+            <div className=" w-full mt-[30px] md:mt-[80px] flex flex-col mb-20 gap-2">
               {items.rings.map((ring) => (
                 <div
                   onClick={() => setActiveRing(ring.id)}
                   key={ring.id}
                   className={` ${activeRing == ring.id ? 'border border-black transition-all duration-100' : ''} w-full flex justify-between items-start p-2 hover:ring hover:ring-gray-100 rounded-md cursor-pointer transition-all duration-200 `}>
                   <div className="flex items-center gap-4">
-                    <img src={ring.image} alt={ring.title} className="h-[100px] w-[100px] rounded-md bg-gray-200" />
+                    <img src={ring.image} alt={ring.title} className="h-[90px] md:h-[100px] w-[100px] rounded-md bg-gray-200" />
                     <div>
                       <h2 className="text-sm font-bold">{ring.title}</h2>
                       <p className="text-sm text-gray-400">${ring.price}</p>
@@ -235,7 +243,7 @@ const Hero = () => {
               ))}
             </div>
 
-            <div className=" w-full flex justify-center items-center mt-36 ">
+            <div className=" w-full flex justify-center items-center md:mt-24 ">
               <button
                 onClick={() => handleSetActiveRing()}
                 className="mx-auto bg-black-300 hover:bg-black transition-colors duration-300 text-white text-sm font-semibold w-full h-10 rounded-md">
@@ -268,7 +276,7 @@ const Hero = () => {
               ))}
             </div>
 
-            <div className=" w-full flex justify-center items-center mt-36 ">
+            <div className=" w-full flex justify-center items-center mt-[170px] lg:mt-[300px] ">
               <button
                 onClick={() => handleSetActiveBox()}
                 className="mx-auto bg-black-300 hover:bg-black transition-colors duration-300 text-white text-sm font-semibold w-full h-10 rounded-md">
